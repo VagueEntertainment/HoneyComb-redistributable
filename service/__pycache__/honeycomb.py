f3be940836080de473b1cd2c071ac96582f8e30d
Benjamin Flanagin <bflanagin@gmail.com>
2021-05-16 21:30:36 -0500

    moving files

---
+++ service/__pycache__/honeycomb.py
@@ -0,0 +1,176 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+import os
+import json
+import time
+import requests
+import asyncio
+
+sys.path.append("..")
+
+import honeycomb_settings as Settings
+import honeycomb_db as DataBase
+import honeycomb_hive as Hive
+
+settings = Settings.get_settings()
+
+HTML_PROCESS = ''
+WebSocket_PROCESS = ''
+uptime = 0
+timeoffset = 3
+pid_recorded = False
+
+def launch_html():
+    
+    subprocess.DETACHED_PROCESS = True
+    try:
+       HTML_PROCESS = subprocess.Popen(['./HoneyComb-redistributable/service/honeycomb_html.py'],stdout=subprocess.PIPE, stderr=subprocess.PIPE,start_new_session=True)
+    except:
+       HTML_PROCESS = subprocess.Popen(['/app/bin/HoneyComb-redistributable/service/honeycomb_html.py'],stdout=subprocess.PIPE, stderr=subprocess.PIPE,start_new_session=True)
+    
+    print("Html Launched ",HTML_PROCESS.pid)
+
+    return HTML_PROCESS
+
+def launch_WebSocket():
+    subprocess.DETACHED_PROCESS = True
+    try:
+        WebSocket_PROCESS = subprocess.Popen(['./HoneyComb-redistributable/service/honeycomb_websocket.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE,start_new_session=True)
+    except:
+        WebSocket_PROCESS = subprocess.Popen(['/app/bin/HoneyComb-redistributable/service/honeycomb_websocket.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE,start_new_session=True)
+        
+    print("WebSocket Launched ",WebSocket_PROCESS.pid)
+
+    return WebSocket_PROCESS
+
+
+def honeycomb_update(account = "none"):
+
+    DataBase.create_db()
+    # Get the latest recorded transaction for account in database
+    hive = ""
+    latest = DataBase.check_latest("honeycomb","history")
+    lastrecord = 0
+    lastchain = 0
+    if latest["honeycomb"]["table"] == "history":
+        lastrecord =latest["honeycomb"]["data"]
+
+    # Get the latest recorded transaction for the account on the chain
+        
+    lastchain = Hive.get_transaction_count(account)
+      
+    if lastrecord < lastchain:
+        #print("running update")
+        if lastrecord == -1:
+            #print("first time, building complete index, This may take a while")
+            for i in Hive.get_history(account,-1): 
+                DataBase.add_history(account,i)
+        else:
+            #print("updating the database by",lastchain-lastrecord,"records")
+            for i in Hive.get_history(account,lastchain-lastrecord):
+                #print("On Hive",i[0])
+                latest = DataBase.check_latest("honeycomb","history")["honeycomb"]["data"]
+               # print("In Database",latest)
+                if i[0] > latest:
+                    DataBase.add_history(account,i)
+       
+    return 1
+    
+def gather_profile_data(account = "none", update = False):
+    
+    DataBase.create_db()
+    userinfo = DataBase.check_latest("honeycomb","account",["ACCOUNT",account])
+
+    if userinfo["honeycomb"]["data"] == -1 or update == True:
+        profile = Hive.get_from_hive(["full_account"],account)
+        balances = Hive.get_from_hive(["balances"],account)
+        followers = Hive.get_from_hive(["followers"],account)
+        following = Hive.get_from_hive(["following"],account)
+        delegation = Hive.get_from_hive(["delegation"],account)
+        DataBase.set_account_info(account,"profile",json.dumps(profile["account"]["json_metadata"]["profile"]))
+        
+        available = balances["balances"]['available']
+        savings = balances["balances"]['savings']
+        
+        print(account)
+        print(update)
+        print(available)
+        
+        DataBase.set_account_info(account,"HBD",json.dumps({"available":available["HBD"],"savings":savings["HBD"]}))
+        DataBase.set_account_info(account,"HIVE",json.dumps({"available":available["HIVE"],"savings":savings["HIVE"]}))
+        DataBase.set_account_info(account,"VESTS",json.dumps({"available":available["VESTS"],"savings":0}))
+        DataBase.set_account_info(account,"LAST_TRANSACTION",Hive.get_transaction_count(account))
+        DataBase.set_account_info(account,"FOLLOWERS",str(followers["followers"]).split("[")[1].split("]")[0].replace("'",""))
+        DataBase.set_account_info(account,"FOLLOWING",str(following["following"]).split("[")[1].split("]")[0].replace("'",""))
+        DataBase.set_account_info(account,"DELEGATION",json.dumps(delegation["delegation"]))
+        
+    return 1
+    
+def gather_dynamic_props(update = False):
+    DataBase.create_db()
+    exists = DataBase.check_latest("honeycomb","misc",["SEARCH_KEY",'hive_dgp'])
+    if exists["honeycomb"]["data"] == -1 or update == True:
+        dynamic_ops = Hive.get_dynamic_global_properties()
+        #print(dynamic_ops)
+        DataBase.add_misc(["hive_dgp",dynamic_ops,"global"])
+
+def pid_record(pid1,pid2):
+    home = os.environ['HOME']
+    if Settings.get_platform() == "Linux":
+        if not os.path.exists(home+"/.config/HoneyComb"):
+            os.mkdir(home+"/.config/HoneyComb")
+        clear = open(home+"/.config/HoneyComb/honeycomb_pids","w")
+        clear.write("")
+        clear.close()
+        pid_file = open(home+"/.config/HoneyComb/honeycomb_pids","a")
+        p = os.getpid()
+        pids = {"pids":{"service":p,"html":pid1,"websocket":pid2}}
+        pid_file.write(json.dumps(pids))
+        pid_file.close()
+  
+if __name__ == "__main__":   
+    try:
+        while True:
+            uptime +=10
+            #print(uptime)
+            if HTML_PROCESS == '':
+                HTML_PROCESS = launch_html()
+           
+            if WebSocket_PROCESS == '':
+                WebSocket_PROCESS = launch_WebSocket()
+                #pid_record(WebSocket_PROCESS.pid)
+          
+            if HTML_PROCESS != '' and WebSocket_PROCESS != '':
+       
+                if not pid_recorded:
+                    pid_record(HTML_PROCESS.pid,WebSocket_PROCESS.pid)
+                    pid_recorded = True
+                
+                if timeoffset == 3:
+                    gather_dynamic_props(True)
+                    accounts = DataBase.get_accounts()
+                    for a in accounts.keys():
+                        gather_profile_data(a,True)
+                        honeycomb_update(a)
+                    timeoffset = 10
+                else:
+                    if uptime % 2400 == 0:
+                        #print("Updating Dynamic Props")
+                        gather_dynamic_props(True)
+           
+                    elif uptime % 1200 == 0:
+                        #print("Updating Profile")
+                        accounts = DataBase.get_accounts()
+                        for a in accounts.keys():
+                            gather_profile_data(a,True)
+                            honeycomb_update(a)
+            time.sleep(timeoffset)
+       
+    except KeyboardInterrupt:
+
+        HTML_PROCESS.kill()
+        WebSocket_PROCESS.kill()
+        sys.exit()
+
---
+++ service/__pycache__/honeycomb_core.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+import os
+import sqlite3
+import json
+import time
+import signal
+
+sys.path.append("..")
+sys.path.append("../service")
+
+import honeycomb_hive as Hive
+import honeycomb_settings as Settings
+import honeycomb_hive_engine as Hive_Engine
+import honeycomb_db as Database
+
+
+from bottle import route, run, template, request, static_file
+
+settings = Settings.get_settings()
+
+
+def shutdown():
+    home = os.environ['HOME']
+    if Settings.get_platform() == "Linux":
+        clear = open(home+"/.config/HoneyComb/honeycomb_pids","r")
+        pids = clear.read()
+        processes = json.loads(pids)
+        if processes["pids"]["service"]:
+            os.kill(processes["pids"]["service"],signal.SIGTERM)
+        if processes["pids"]["html"]:
+            os.kill(processes["pids"]["html"],signal.SIGTERM)
+        if processes["pids"]["websocket"]:
+            os.kill(processes["pids"]["websocket"],signal.SIGTERM)
+            
+        clear.close()
+        
+
+
+
+def message(interface,data):
+    response = {"honeycomb":{"code":100,"msg":"Please issue a command to continue"}}
+    #print(interface,data)
+    try:
+        from_client = json.loads(data)
+    except:
+        print("failed to parse "+data)
+        return json.dumps({"honeycomb":{"code":101,"msg":"messages must be in json formated string"}})
+    else:
+        act = from_client["act"]
+        response ={"honeycomb":{"code":200,"type":from_client["type"]}}
+        for a in act:
+        
+        # From local stores
+        
+        # Settings file
+            if a == "get_settings" and interface == "service":
+                if "settings" not in response:
+                    response["honeycomb"]["settings"] = {}
+                response["honeycomb"]["settings"].update(Settings.get_settings())
+            elif a == "check_settings" and interface == "service":
+                if "settings" not in response:
+                    response["honeycomb"]["settings"] = {}
+                response["honeycomb"]["settings"]["file"] = Settings.check_settings_file()
+            elif a == "save_settings" and interface == "service":
+                if "settings" not in response:
+                    response["honeycomb"]["settings"] = {}
+                response["honeycomb"]["settings"].update(Settings.save_settings(from_client["passphrase"]))
+            
+        # Database functions
+        
+            if a == "get_history" and interface == "service":
+                response["honeycomb"]["database"] = Database.get_from_history(from_client["opts"],from_client["account"])
+           
+            elif a == "get_app_data" and interface == "service":
+                response["honeycomb"]["database"] = Database.get_from_app()
+                
+        # Account functions
+            elif a == "wallet_exists" and interface == "service":
+                response["honeycomb"]["hive"] = Hive.hive_wallet_exists()
+                
+            elif a ==  "create_wallet" and interface == "service":
+                response["honeycomb"]["hive"] = Hive.hive_create_wallet(from_client["passphrase"])
+                    
+            elif a == "add_account" and interface == "service":
+                wallet = Hive.import_account(from_client["account"],from_client["keys"],from_client["passphrase"])
+                #print(wallet)
+                profile = Hive.get_from_hive(["profile"],from_client["account"],[])
+               # print(profile)
+                if wallet["wallet"] == "imported":
+                    Database.set_account_info(from_client["account"],"profile",json.dumps(profile["profile"]))
+                response["honeycomb"]["hive"] = wallet
+                    
+            elif a == "get_accounts" and interface == "service":
+                response["honeycomb"]["accounts"] = Database.get_accounts()
+                
+            elif a == "get_account" and interface == "service":
+                if "params" in from_client.keys():
+                    types = from_client["params"]["get_account"]
+                    response["honeycomb"]["database"] = Database.get_from_account(from_client["account"],types)
+                else:
+                    response["honeycomb"]["database"] = Database.get_from_account(from_client["account"],from_client["opts"])
+            
+        # Direct from Hive
+       
+            elif a == "import_account" and interface == "service":
+                response["honeycomb"]["hive"] = Hive.import_account(from_client["account"],from_client["keys"],from_client["passphrase"])
+            elif a == "get_from_hive" and interface == "service":
+                 if "params" in from_client.keys():
+                    types = from_client["params"]["get_from_hive"]
+                    response["honeycomb"]["hive"] = Hive.get_from_hive(types,from_client["account"],from_client["opts"])
+                 else:
+                    response["honeycomb"]["hive"] = Hive.get_from_hive(from_client["opts"],from_client["account"],from_client["opts"])
+            elif a == "get_hive_dynamic_props" and interface == "service":
+                #response = Hive.get_dynamic_global_properties()
+                response["honeycomb"]["hive"] = Database.get_from_misc(['hive_dgp'])
+            elif a == "claim_hive_rewards" and interface == "service":
+                response["honeycomb"]["hive"] = Hive.claim_hive_rewards(from_client["account"],from_client["opts"])
+            
+        # Direct from Hive Engine
+        
+            elif a == "hive_engine_search" and interface == "service":
+                response["honeycomb"]["hive_engine"] = Hive_Engine.find_on_hive_engine(from_client["query"],from_client["token"],from_client["opt"])
+            elif a == "get_nftshowroom_art" and interface == "service":
+                response["honeycomb"]["hive_engine"] = {"nftshowroom":Hive_Engine.get_nftshowroom_art(from_client["account"],from_client["opts"])}
+                #print(response)
+            elif a == "get_from_hive_engine" and interface == "service":
+                if "params" in from_client.keys():
+                    types = from_client["params"]["get_from_hive_engine"]
+                    response["honeycomb"]["hive_engine"]= Hive_Engine.get_from_hive_engine(types,from_client["account"])
+                else:
+                    response["honeycomb"]["hive_engine"]= Hive_Engine.get_from_hive_engine(from_client["opts"],from_client["account"])
+        
+        # HoneyComb Specific
+            
+        # To Hive
+        
+            elif a == "register_client" and interface == "service":
+                response["honeycomb"]["to_hive"] = Hive.register_client()
+            elif a == "update_loc" and interface == "service":
+                response["honeycomb"]["to_hive"] = Hive.update_client_loc(from_client["appname"])
+            
+        # From Hive
+        
+            elif a == "registration_check" and interface == "service":
+                if Hive.registration_check(from_client["account"]):
+                    response["honeycomb"]["registration"] = True
+                else:
+                    response["honeycomb"]["registration"] = False
+        
+            elif a == "image_retrieval" and interface == "service":
+                response = Database.image_retrieval(from_client["url"])        
+                
+            elif a == "shutdown_service" and interface == "service":
+                response["honeycomb"]["system"] ={"message":"shutdown"}
+                shutdown()
+                
+        if a != "image_retrieval":
+            #print(response)
+            return json.dumps(response)
+        else:
+            return response
